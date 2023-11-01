3a312afdb3664e898619d0264abf34eca2367941
Ali Aboshady <ali_aboshady@hotmail.com>
2023-11-01 03:09:43 +0000

    Added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import Header from './components/header/Header';
+import Layout from '../src/components/Layout';
+
 export default function App() {
-  return <div></div>;
+  return (
+    <>
+      <div className="-z-10 bg-[#FBE2E3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+      <div className="-z-10 bg-[#DBD7FB] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+
+      <Header />
+      <Layout />
+    </>
+  );
 }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,9 @@
+import Home from './home/Home';
+
+export default function Layout() {
+  return (
+    <main className="absolute flex flex-col items-center w-full">
+      <Home />
+    </main>
+  );
+}
