c562edf1c4bce396c935e1fa38b8ed2de3b7095d
Ali Aboshady <ali_aboshady@hotmail.com>
2023-11-06 16:13:14 +0000

    Contact form sends emails to me and the sender

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,14 +1,48 @@
 import { motion } from 'framer-motion';
+import { useRef } from 'react';
 import useSectionInView from '../../hooks/useSectionInView';
 import SectionHeading from '../SectionHeading';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
-  const ref = useSectionInView('Contact');
+  const serviceID = 'service_dghdb8p';
+  const templateID_ToMe = 'template_99iyvin';
+  const templateID_ToFormSender = 'template_bv463j9';
+  const publicKey = 'sMnYF7E84nMTIwjWH';
+
+  const refSection = useSectionInView('Contact');
+  const refForm = useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(serviceID, templateID_ToMe, refForm.current, publicKey)
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    emailjs
+      .sendForm(serviceID, templateID_ToFormSender, refForm.current, publicKey)
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
 
   return (
     <motion.section
       id="contact"
-      ref={ref}
+      ref={refSection}
       className="scroll-mt-28 sm:mb-40 mb-20 w-[min(100%, 38rem)] text-center"
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
@@ -25,14 +59,32 @@ export default function Contact() {
         or through this form.
       </p>
 
-      <form className="flex flex-col mt-10">
+      <form
+        ref={refForm}
+        className="flex flex-col mt-10"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          name="user_name"
+          placeholder="Your full name"
+          required
+          maxLength={500}
+          className="h-14 border-black/10 px-4 mb-3 border rounded-lg"
+        />
         <input
           type="email"
+          name="user_email"
           placeholder="Your email address"
+          required
+          maxLength={500}
           className="h-14 border-black/10 px-4 border rounded-lg"
         />
         <textarea
+          name="message"
           placeholder="Your message"
+          required
+          maxLength={2000}
           className="h-52 border-black/10 p-4 my-3 border rounded-lg"
         />
         <button
