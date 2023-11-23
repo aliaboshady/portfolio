4608e89bd110cf4137299695d160d0a4c2bcf28d
Ali Aboshady <ali_aboshady@hotmail.com>
2023-11-23 21:17:33 +0000

    Edited City Stamp

--- data/data.jsx
+++ data/data.jsx
@@ -154,11 +154,11 @@ export const projectsData = [
     imageName: 'SaucySlice.png',
   },
   {
-    title: 'WorldWise',
+    title: 'City Stamp',
     description:
       'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
     tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
-    imageName: 'WorldWise.png',
+    imageName: 'CityStamp.png',
   },
 ];
 
